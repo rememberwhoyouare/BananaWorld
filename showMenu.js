21be34ebf0e51c22929f42d4bd2286d2b55d65b7
Polly <polly@Pollys-MacBook-Air.local>
2022-01-24 15:55:03 +0300

    new updated version

--- showMenu.js
+++
@@ -1,8 +0,0 @@
-function mobileMenuSwitch() {
-    let menu = document.getElementsByClassName("nav-menu")[0];
-  
-    menu.className = 
-      menu.className.includes("show") ?
-      "nav-menu":
-      "nav-menu show";
-}
